edf149eaeab7110596536c3529143fc4538d5cfc
Tanvi parikh <tapa8728@colorado.edu>
2017-10-01 18:43:18 -0700

    Add node after specific node skeleton

---
+++ Linked List/Insertion/LL-Insertion-after-specific-node.py
@@ -0,0 +1,39 @@
+# Program to insert a node after a node of specific value
+
+class Node(object):
+    
+    def __init__(self, val):
+        self.data = val
+        self.next = None
+    
+class LinkedList(object):
+    
+    def __init__(self):
+        self.head = None
+        self.tail = None
+        
+    def isEmpty(self):
+        pass
+    
+    def status(self):
+        pass
+    
+    def listLength(self):
+        pass
+    
+    def traverse(self):
+        pass
+    
+    def addBeg(self):
+        pass
+    
+    ## Find:
+    # Find the node of specific value and return it.
+    # if node not found, return None
+    def find(self, val):
+        pass
+    
+    ## Add's new node after a node of specific value
+    # If node not found, add it at the beginning. 
+    def addNodeVal(self, val1, val2):
+        pass
